029fbb3b8940390c1a4e6842edadef474ac16d95
Julia Kurganskaia <17925683+julia-kurganskaia@users.noreply.github.com>
2021-02-10 12:04:56 +1300

    Able to enter a new subreddit to view the titles

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
 function LoadSubreddit ({ children, dispatch }) {
+  const [newSubreddit, setNewSubreddit] = useState("");
+
+    const handleChange = (event) => {
+      setNewSubreddit(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      dispatch(fetchPosts(newSubreddit))
+      setNewSubreddit("");
+    }
+
   return (
     <div>
       <button onClick={() => dispatch(fetchPosts('newzealand'))}>
     Fetch Posts
       </button>
       {children}
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="value" onChange={handleChange} value={newSubreddit} />
+        <button>View a new subbreddit</button> 
+      </form>
+      
     </div>
   )
 }
