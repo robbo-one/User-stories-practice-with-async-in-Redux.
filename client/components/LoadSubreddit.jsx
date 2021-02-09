bd5d3cba64101783d0f76a20abde9d8f63272372
Michael Parkin <73506758+mike-parkin@users.noreply.github.com>
2021-02-10 11:16:39 +1300

    search subreddits implemented

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
 function LoadSubreddit ({ children, dispatch }) {
+  
+  const [ subreddit , setSubreddit ] = useState('newzealand')
+
+  const handleChange = (e) => {
+    setSubreddit(e.target.value)
+  }
+
   return (
     <div>
-      <button onClick={() => dispatch(fetchPosts('newzealand'))}>
+      <input type='text' name='name' onChange={handleChange} placeholder='newzealand'/>
+      <button onClick={() => dispatch(fetchPosts(subreddit))}>
     Fetch Posts
       </button>
       {children}
