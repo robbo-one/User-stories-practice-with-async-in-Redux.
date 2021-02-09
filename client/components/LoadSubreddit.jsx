7dd6b212eac8e4fd79eaa3e70efb958effc59e21
student <student@eda-bc-02.localdomain>
2021-02-10 12:35:36 +1300

    add new subreddit

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
 function LoadSubreddit ({ children, dispatch }) {
+
+  const [ subreddit, setSubreddit ] = useState('newzealand')
+
+  const handlechange = (event) => {
+    setSubreddit(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(fetchPosts(subreddit))
+  }
   return (
     <div>
       <button onClick={() => dispatch(fetchPosts('newzealand'))}>
     Fetch Posts
       </button>
       {children}
+    <form onSubmit={handleSubmit}>
+    <input type='text' name='name' onChange={handlechange} placeholder='newzealand'/>
+    <button>New Subreddit</button>
+    </form>
     </div>
   )
 }
