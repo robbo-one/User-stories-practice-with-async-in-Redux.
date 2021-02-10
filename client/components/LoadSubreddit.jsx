3a0837aaa22c0eb8212526639369f1e41101fae9
Isaac Sedcole <44042755+Isaac-Sedcole@users.noreply.github.com>
2021-02-10 15:51:13 +1300

    add handle change to form

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
+
 function LoadSubreddit ({ children, dispatch }) {
+
+  let [subreddit, setSubreddit] = useState("")
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    setSubreddit = e.target.value
+    console.log(setSubreddit)
+  }
+
   return (
     <div>
-      <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-    Fetch Posts
-      </button>
+      <form onSubmit={(e) => {e.preventDefault() ; dispatch(fetchPosts('newzealand'))}} >
+        <label>Enter Subreddit:</label>
+        <input onChange={(e) => handleChange(e)} type="text" name="subreddit" placeholder="WTF?"></input>
+        <input type="submit" value="Get Posts!"></input>
+      </form>
       {children}
     </div>
   )
