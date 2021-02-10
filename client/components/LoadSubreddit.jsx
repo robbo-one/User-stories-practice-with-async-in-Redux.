bd31657b76a5f7ff0bdb36de468ae08fbb19b6aa
Isaac Sedcole <44042755+Isaac-Sedcole@users.noreply.github.com>
2021-02-10 16:04:44 +1300

    ad handle click to form

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -4,24 +4,27 @@ import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
 
-function LoadSubreddit ({ children, dispatch }) {
+function LoadSubreddit (props) {
 
   let [subreddit, setSubreddit] = useState("")
 
   const handleChange = (e) => {
     e.preventDefault()
     setSubreddit = e.target.value
-    console.log(setSubreddit)
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    props.dispatch(fetchPosts(setSubreddit))
   }
 
   return (
     <div>
-      <form onSubmit={(e) => {e.preventDefault() ; dispatch(fetchPosts('newzealand'))}} >
+      <form onSubmit={(e) => {e.preventDefault() ; dispatch(fetchPosts({setSubreddit}))}} >
         <label>Enter Subreddit:</label>
         <input onChange={(e) => handleChange(e)} type="text" name="subreddit" placeholder="WTF?"></input>
-        <input type="submit" value="Get Posts!"></input>
+        <input onClick={(e) => handleClick(e)} type="submit" value="Get Posts!"></input>
       </form>
-      {children}
     </div>
   )
 }
